d446a6793bd8b512fe883e12e7debd331b961125
Vito Farenga <vitofarenga@libero.it>
2022-06-16 17:01:48 +0200

    buzzfizz funzionante

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 for (let index = 1; index <= 100; index++){
-    
-
-    if (index % 3 == 0){
+    if ((index % 3 == 0) && (index % 5 == 0)){
+        console.log("BuzzFizz");
+    }
+    else if (index % 3 == 0){
         console.log("Buzz");
     }
     else if (index % 5 == 0){
         console.log("Fizz");
     }
     else {
-        console.log(index)
+        console.log(index);
     }
+    
 }
