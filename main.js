ee00da8d24353c2380a09504fc34af87064feeca
Vito Farenga <vitofarenga@libero.it>
2022-06-16 16:03:05 +0200

    fizz e buzz funzionano, per fizzbuzz sto facendo prove, lo so non posso dividere un numero con una stringa

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 for (let i = 1; i <= 100; i = i + 1){
     console.log(i)
+    if (i % 3 == 0)
+    console.log("Fizz")
+    else if (i % 5 == 0)
+    console.log("Buzz")
+    else if (i % "Fizz", "Buzz" == 0)
+    console.log("FizzBuzz")
 }
