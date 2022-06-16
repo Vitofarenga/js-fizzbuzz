994d885be8941676744f135110d133f8d0c35513
Vito Farenga <vitofarenga@libero.it>
2022-06-16 16:34:34 +0200

    buzz e fizz funzionanti

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 for (let index = 1; index <= 100; index++){
     console.log(index)
+
+    if (index % 3 == 0){
+        console.log("Buzz")
+    }
+    else if (index % 5 == 0){
+        console.log("Fizz")
+    }
     
     
 }
