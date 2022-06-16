7072177ae153b6e7689fd9061671a9fbe9703ed7
Vito Farenga <vitofarenga@libero.it>
2022-06-16 16:27:12 +0200

    rifacendo-tutto-for-base

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-for (let i = 1; i <= 100; i = i + 1){
-    
+for (let index = 1; index <= 100; index++){
     console.log(i)
-    if (i % 3 == 0)
-    console.log("Fizz")
-    const fizz = (i % 3 == 0)
-    if (i % 5 == 0)
-    console.log("Buzz")
-    const buzz = (i % 5 == 0)
-    if (i % fizz, buzz  == 0)
-    console.log("FizzBuzz")
+    
+    
 }
